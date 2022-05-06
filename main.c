7d3de3f985a049716083e03bf903fa30537ae172
psw03180@naver.com <psw03180@naver.com>
2022-05-06 22:06:35 +0900

    init

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
-// main.c
+#include <stdio.h>
+#include "sosil1.h"
+#include "sosil2.h"
+#include "sosil3.h"
+#include "sosil4.h"
+#include "sosil5.h"
 
-#include "diary.h"
-int main()
+extern void func1();
+extern void func2();
+extern void func3();
+extern void func4();
+extern void func5();
+
+main()
 {
-	memo();
-	calendar();
-	return 0;
+	func1();
+	func2();
+	func3();
+	func4();
+	func5();
 }
-
